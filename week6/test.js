6fc3dfb06bfa59ded3459fcff2c0f1e3d08fe933
drsshk <drsshk@yahoo.com.my>
2024-03-28 16:32:52 +0800

    update all

---
+++ week6/test.js
@@ -0,0 +1,16 @@
+const nombor5 = 5
+const nombor10 = 10
+
+function tambahDua (nombor1, nombor2) {
+    return nombor1 + nombor2
+}
+
+const hasil = tambahDua(nombor5, nombor10)
+console.log(hasil);
+
+
+function convertUsdToMyr (hargaUsd) {
+    return hargaUsd * 4.6;
+}
+
+console.log('first', first)
